b474ec20fc512d56864adbbae56a52223ef8f26c
billy <ccs99817@gmail.com>
2025-02-26 15:25:27 -0500

    Support .erb formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,26 +2,31 @@
 -- Don't forget to install linter and formatter of each language in :Mason
 
 return {
-	"nvimtools/none-ls.nvim",
-    dependencies = {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
     "nvimtools/none-ls-extras.nvim",
   },
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
         -- Lua
-				null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.stylua,
         -- Ruby
-				null_ls.builtins.diagnostics.rubocop,
-				null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.formatting.rubocop,
         -- JavaScript
-				null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettier,
         require("none-ls.diagnostics.eslint_d"),
+        -- Erb
+        -- Required: gem install erb-formatter
+        null_ls.builtins.formatting.erb_format.with({
+          filetypes = { "eruby" },
+        }),
         -- Genereal
-				null_ls.builtins.completion.spell,
-			},
-		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+        null_ls.builtins.completion.spell,
+      },
+    })
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
